c6b69baa9ea36658d84a80b4d05575e8b488b929
Manuel Stadelmann <manuel@allotria.ch>
2015-02-16 21:38:28 +0100

    added Oracle Java 7 and 8

--- bootstrap_ci.sh
+++ bootstrap_ci.sh
@@ -8,6 +8,19 @@ sudo echo "deb http://pkg.jenkins-ci.org/debian binary/" >> /etc/apt/sources.lis
 apt-get update
 apt-get install -y apache2 default-jdk maven jenkins git unzip
 
+# Install Oracle Java 7 and 8
+# (thanks to https://gist.github.com/tinkerware/cf0c47bb69bf42c2d740)
+apt-get -y -q update
+apt-get -y -q upgrade
+apt-get -y -q install software-properties-common htop
+add-apt-repository ppa:webupd8team/java
+apt-get -y -q update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-get -y -q install oracle-java8-installer
+apt-get -y -q install oracle-java7-installer
+update-java-alternatives -s java-8-oracle
+
 # Install Artifactory
 #sudo cp /vagrant/artifactory-3.5.1.zip /opt
 pushd /opt
