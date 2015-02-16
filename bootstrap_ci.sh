ab07b75ee12cc3c26fd42c6e71a6bd7a44a3e11a
Manuel Stadelmann <manuel.stadelmann@arbetsformedlingen.se>
2015-02-16 15:07:05 +0100

    Added Nexus installation and some environment variables

--- bootstrap_ci.sh
+++ bootstrap_ci.sh
@@ -6,14 +6,17 @@ sudo echo "deb http://pkg.jenkins-ci.org/debian binary/" >> /etc/apt/sources.lis
 
 # Basic stuff
 apt-get update
-apt-get install -y apache2 jenkins git unzip
+apt-get install -y apache2 default-jdk jenkins git unzip
 
 # Install Artifactory
 #sudo cp /vagrant/artifactory-3.5.1.zip /opt
 pushd /opt
 sudo wget -q -O artifactory-3.5.1.zip http://bit.ly/Hqv9aj
 sudo unzip artifactory-3.5.1.zip
+sudo rm -f artifactory-3.5.1.zip
+sudo ln -s `ls -1d artifactory-*` artifactory
 sudo artifactory-3.5.1/bin/installService.sh
+sudo cp /vagrant/artifactory_server.xml /opt/artifactory/tomcat/conf/server.xml
 sudo service artifactory start
 popd
 
@@ -22,7 +25,17 @@ popd
 pushd /usr/local
 sudo wget -q -O nexus-latest-bundle.tar.gz http://www.sonatype.org/downloads/nexus-latest-bundle.tar.gz
 sudo tar xvzf nexus-latest-bundle.tar.gz
-
-
-
+sudo rm -f nexus-latest-bundle.tar.gz
+sudo ln -s `ls -1d nexus-*` nexus
+sudo cp /vagrant/nexus /etc/init.d/nexus
+sudo chown root /etc/init.d/nexus
+sudo chmod 755 /etc/init.d/nexus
+pushd /etc/init.d
+sudo update-rc.d nexus defaults
+sudo service nexus start
 popd
+popd
+
+
+# Set environment variables
+sudo cp /vagrant/environment /etc/environment
